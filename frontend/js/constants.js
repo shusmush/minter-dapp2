83f0869dd33113c698a05dfab26793c18c07c4d6
shusmush <102856526+shusmush@users.noreply.github.com>
2022-04-14 11:59:37 +0100

    2 14

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,24 +1,24 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x2327e488327487ab22E6227B09B8Fc3c3A5b57B2"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_h1 = "Welcome to the DPP gang   - Let's Ape In?!!";
+const welcome_h2 = "Connect to MetaMask to Get Apeing";
+const welcome_p = 'Join us on Discord for all the details of DEGENERATE POKER PRIMATES NFTs. DPP is designed to be the most rewarding poker-related NFT project, with guaranteed benefits (Live and Online) up until August 2025!';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
+const p_presale_coming_soon = "We are hard at work for the upcoming launch of this exciting NFT Collection. Stay tuned on DISCORD for all project details and updates!";
+const p_presale_mint_not_whitelisted = "Unfortunately, you have not been whitelisted for the pre-sale.. 😢";
+const p_presale_mint_whitelisted = "CONGRATS!! You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
 const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_whitelisted = "Mint Your Primate NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
-const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
+const button_presale_already_minted = "Join Our Community";
+const button_public_mint = "Mint your NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
 const abi = [
