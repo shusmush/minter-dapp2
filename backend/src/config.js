7f784ad69efd6b15ea94012c32c9f58436dc151c
shusmush <102856526+shusmush@users.noreply.github.com>
2022-04-19 12:59:21 +0100

    change website

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "https://dgenpokerprimates.com", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://dgenpokerprimates.co", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
